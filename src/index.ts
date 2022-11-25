42000440978319cdbe6eb3f6f8ccc3567b321617
Raphael Serafim <raphaelvserafim@gmail.com>
2022-11-24 22:08:53 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow() {
+
+    const mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "../views/index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
